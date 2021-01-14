4352278c69b54e55ca6c298997feb2016c1a879c
Pius Alfred <me.pius1102@gmail.com>
2021-01-14 15:18:53 +0300

    pp commands templates

--- cli/root.go
+++ cli/root.go
@@ -29,13 +29,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pp",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A simple commandline tool to store password on your laptop",
+	Long: `pp is a really dump tool it does no magic, It just store passwords
+and helps you retrieve them later easily`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
@@ -53,15 +49,12 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	rootCmd.PersistentFlags().StringP("name","n","","name of account")
+	rootCmd.PersistentFlags().StringP("username","u","","account username")
+	rootCmd.PersistentFlags().StringP("email","e","","email used")
+	rootCmd.PersistentFlags().StringP("password","p","","password")
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pp.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
