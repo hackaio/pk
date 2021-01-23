691d07d959bf672d777d3c972552f7f57aee12c4
Pius Alfred <me.pius1102@gmail.com>
2021-01-23 12:34:56 +0300

    add credstore interface [issue #1]

---
+++ credstore.go
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2021 PIUS ALFRED me.pius1102@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pk
+
+
+//CredStore provides API to manage credentials securely
+//per OS specific mechanism
+type CredStore interface {
+	Set(lbl, url, user, secret string)error
+	Get(lbl, url string)(string,error)
+	Del(lbl, url string)error
+}
